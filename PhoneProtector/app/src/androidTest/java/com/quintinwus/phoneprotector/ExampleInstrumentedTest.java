7a94fc981a98933605e8bbcc131208c853dc592f
Shurrik <272775600@qq.com>
2019-02-22 19:00:51 +0800

    init the config of Phone Protector

--- PhoneProtector/app/src/androidTest/java/com/quintinwus/phoneprotector/ExampleInstrumentedTest.java
+++ PhoneProtector/app/src/androidTest/java/com/quintinwus/phoneprotector/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.sharkwang8.phoneassistant", appContext.getPackageName());
+        assertEquals("com.sharkwang8.phoneprotector", appContext.getPackageName());
     }
 }
